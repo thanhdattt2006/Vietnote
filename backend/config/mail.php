357944e037e0ae2418258d1f656862e54ef29db6
thanhdattt2006 <thanhdattt2006@gmail.com>
2025-12-03 00:44:20 +0700

    Mail change

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -49,6 +49,10 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'resend' => [
+            'transport' => 'resend',
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
