54d99d02d7f10c6409fc34dcbe3289dad1b96c33
thanhdattt2006 <thanhdattt2006@gmail.com>
2025-12-03 00:00:23 +0700

    Fix vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // Frontend chạy port 3000
-    proxy: {
-      // MA THUẬT Ở ĐÂY:
-      // Hễ React gặp đường dẫn bắt đầu bằng /storage (ví dụ: <img src="/storage/abc.jpg">)
-      // Nó sẽ tự động chuyển hướng request đó sang http://localhost:8000/storage/abc.jpg
-      '/storage': {
-        target: 'https://vietnote.onrender.com',
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   // MA THUẬT Ở ĐÂY:
+    //   // Hễ React gặp đường dẫn bắt đầu bằng /storage (ví dụ: <img src="/storage/abc.jpg">)
+    //   // Nó sẽ tự động chuyển hướng request đó sang http://localhost:8000/storage/abc.jpg
+    //   '/storage': {
+    //     target: 'https://vietnote.onrender.com/',
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 });
